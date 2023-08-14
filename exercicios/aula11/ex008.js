98ac597b09a1dedef74c26a30381e174e89e96d1
Adriano de Oliveira <130682278+Adriianooliveira@users.noreply.github.com>
2023-08-14 16:19:21 -0300

    novos exercícios adicionados

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50.3
+console.log('a velocidade do seu carro e ' + vel )
+if (vel > 60) {  //condiçõo simples
+    console.log('voce ultrapassou a velocidade permitida. MULTADO')
+}
+console.log('dirija sempre usando o cinto de segurança')
