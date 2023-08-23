0dea80721ff3d68519c19467ee7f9d69276f54f5
Adriano de Oliveira <130682278+Adriianooliveira@users.noreply.github.com>
2023-08-23 04:54:52 -0300

    novos exercícios adicionados

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,18 @@
+//AULA DE REPETIÇÕES//
+
+var c = 1
+while (c <= 6) {
+    console.log('seja bem vindo ao site')
+    c++
+}
+
+/* do {
+    console.log('seja bem vindo ao site')
+    c++
+} while (c <= 6)
+*/
+
+//WHILE (ENQUANTO alguma coisa for verdade)
+
+// WHILE () teste logico feito no inicio
+// DO {}  WHILE ()  teste logico feito no final
---
+++ exercicios/aula14/ambiente.js
@@ -0,0 +1,12 @@
+// for (inicio ; teste ; incr)
+
+/*var c = 1
+while (c <= 5) {
+    console.log('seja bem vindo ao site')
+    c++
+}
+*/
+
+for (var c = 1; c <= 5; c++ ) {
+    console.log('seja bem vindo ao site')
+}
