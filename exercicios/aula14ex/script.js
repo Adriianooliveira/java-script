7b379e1c16d7c7c0fbd126c2360a51a256c5a64a
Adriano de Oliveira <130682278+Adriianooliveira@users.noreply.github.com>
2023-08-24 04:27:55 -0300

    novos exercícios adicionados

--- exercicios/aula14ex/script.js
+++ exercicios/aula14ex/script.js
@@ -1,14 +1,21 @@
 function contar() {
-    var inicio = window.document.getElementById('inicio')
+    var ini = window.document.getElementById('inicio')
     var fim = window.document.getElementById('fim')
-    var passo = window.document.getElementById('passo')
+    var pass = window.document.getElementById('passo')
     var res = window.document.getElementById('res')
-    var i = Number(inicio.value)
+    var i = Number(ini.value)
     var f = Number(fim.value)
-    //var p = Number(passo.value)
-    while (Number(i <= f)) {
-        res.innerHTML = `contagem ${i} ${f}`
-        c++
+    var p = Number(pass.value)
 
+    if (ini.value.length == 0 || fim.value.length == 0 || pass.value.length == 0) {
+        window.alert('verifique seu codigo')
+
+    } else {
+        res.innerHTML = 'contando'
+
+        for(var n = i; n <= f; n = n + p) {
+            res.innerHTML += `${n}`
+
+        }
     }
 }
