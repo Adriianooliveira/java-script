4a9ce17092428ff059602165908d437a57f6df33
Adriano de Oliveira <130682278+Adriianooliveira@users.noreply.github.com>
2023-08-27 04:55:45 -0300

    novo exercício adicionado

--- exercicios/aula14ex/script.js
+++ exercicios/aula14ex/script.js
@@ -7,14 +7,36 @@ function contar() {
     var f = Number(fim.value)
     var p = Number(pass.value)
 
-    if (ini.value.length == 0 || fim.value.length == 0 || pass.value.length == 0) {
-        window.alert('verifique seu codigo')
+    if (ini.value.length === 0 || fim.value.length === 0 || pass.value.length === 0) {
+        res.innerHTML = 'Impossivel contar'
 
     } else {
-        res.innerHTML = 'contando'
+        res.innerHTML = 'Contando: </br>'
 
-        for(var n = i; n <= f; n = n + p) {
-            res.innerHTML += `${n}`
+        if (p <= 0) {
+            window.alert('Passo invalido considerando o passo 1')
+            p = 1
+
+        }
+
+        if (i < f) {
+            //contagem crescente
+
+            for(var n = i; n <= f; n = n + p) {
+                res.innerHTML += `&#x1f449 ${n} `
+
+            }
+            res.innerHTML += '&#x1f3c1'
+
+
+        } else if (i > f) {
+            //contagem regressiva
+
+            for ( var n = i ; n > f ; n = n - p) {
+                res.innerHTML += `&#x1f449 ${n}`
+
+            }
+            res.innerHTML += '&#x1f3c1'
 
         }
     }
