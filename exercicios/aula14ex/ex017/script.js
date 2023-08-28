77896199c7a301bddc058cf71e6c32bc5efb44e9
Adriano de Oliveira <130682278+Adriianooliveira@users.noreply.github.com>
2023-08-28 05:55:56 -0300

    novos exercícios adicionados

---
+++ exercicios/aula14ex/ex017/script.js
@@ -0,0 +1,20 @@
+function tabuada() {
+    var num = window.document.getElementById('num')
+    var tabuada = window.document.getElementById('itabuada')
+
+    if(num.value.length === 0) {
+        window.alert('verifique seu codigo')
+
+    } else {
+        var nm = Number(num.value)
+        var n = 1
+        tabuada.innerHTML = ''
+        while (n <= 10) {
+            var item = window.document.createElement('option')
+            item.innerHTML += `${nm} x ${n} = ${nm*n}`
+            item.value = `tab${cm}`   
+            tabuada.appendChild(item)
+            n++
+        }
+    }
+}
